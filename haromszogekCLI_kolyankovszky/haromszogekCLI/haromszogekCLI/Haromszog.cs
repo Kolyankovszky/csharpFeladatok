1c5bb7c29a9d994c8bac9736bd84f353cba6f374
Kolyankovszky <kolyankovszkyg@kkszki.hu>
2023-02-24 17:29:32 +0100

    haromszogekConsolosFeladat

---
+++ haromszogekCLI_kolyankovszky/haromszogekCLI/haromszogekCLI/Haromszog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace haromszogekCLI
+{
+    public class Haromszog
+    {
+        public int oldalA { get; private set; }
+        public int oldalB { get; private set; }
+        public int oldalC { get; private set; }
+
+        public Haromszog(string sor)
+        {
+            string[] darabok = sor.Split(' ');
+            this.oldalA = Convert.ToInt32(darabok[0]);
+            this.oldalB = Convert.ToInt32(darabok[1]);
+            this.oldalC = Convert.ToInt32(darabok[2]);
+        }
+    }
+}
---
+++ haromszogekCLI_kolyankovszky/haromszogekCLI/haromszogekCLI/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace haromszogekCLI
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<Haromszog> haromszogs = new List<Haromszog>();
+            StreamReader sr = new StreamReader("haromszogek2.csv");
+            while (!sr.EndOfStream)
+            {
+                Haromszog h = new Haromszog(sr.ReadLine());
+                haromszogs.Add(h);
+            }
+            sr.Close();
+
+            foreach (var item in haromszogs)
+            {
+                if (derekszogu(item.oldalA, item.oldalB, item.oldalC))
+                {
+                    Console.WriteLine("a:"+item.oldalA+" b:"+item.oldalB+" c:"+item.oldalC);
+                }
+            }
+            Haromszog maxharomszog = haromszogs[0];
+            foreach (var item in haromszogs)
+            {
+                if ((item.oldalA*item.oldalB)/2>(maxharomszog.oldalA*maxharomszog.oldalB)/2)
+                {
+                    maxharomszog = item;
+                }
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+        public static bool derekszogu(int a, int b, int c)
+        {
+            if (a*a+b*b==c*c)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
